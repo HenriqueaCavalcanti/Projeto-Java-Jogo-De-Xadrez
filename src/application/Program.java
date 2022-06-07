7557f9e2a91dbf410f925dea8e8a39d82d9bb1ee
Henrique Alves <henriquealves_cavalcanti@hotmail.com>
2022-06-07 20:23:41 -0300

    Classe da posição

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import camadadotabuleiro.Posicao;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Posicao posicao = new Posicao(1, 2);
+        System.out.println(posicao);
     }
 }
